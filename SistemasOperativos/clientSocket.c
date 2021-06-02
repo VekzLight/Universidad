025bc0758c611d50363bffe00bb80ec0803f46e1
vekzlight <dnasystem.vekz@gmail.com>
2021-06-02 04:31:47 -0500

    Separacion Cliente - Servidor y  Añade Monitoreo a subcarpetas

---
+++ SistemasOperativos/clientSocket.c
@@ -0,0 +1,276 @@
+//Simbolos Estandar y salir
+#include <unistd.h>
+#include <stdlib.h> 
+#include <sys/wait.h>
+
+//Errores y Strings
+#include <stdio.h> 
+#include <string.h> 
+#include <cerrno>
+
+//Sockets
+#include <netinet/in.h>
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netdb.h> 
+
+//Monitorizacion
+#include <sys/inotify.h>
+#include <dirent.h>
+
+
+
+
+#define BUFF_SIZE 1024
+
+
+
+void enviarArchivo(int fdConnection, FILE *fdFile, char *name);
+void addWatchToTree(int fdDirectory, char *_route, int wdCreate);
+void buscarArchivo(char *routeRoot, char *name, char *_route);
+void clientSocket(char *_ip, int _port, char *_routeRoot, char *name, int fdDirectory, int wdCreate);
+void directoryMonitor(char *_ip, int _port, char _route[]);
+
+
+
+int main(int argc, char ** argv) { 
+    directoryMonitor(argv[1], atoi(argv[2]), argv[3]);
+    exit(0);
+} 
+
+
+
+void directoryMonitor(char *_ip, int _port, char _route[]){
+    int fdDirectory, wdCreate;      // File Descriptor de la carpeta a monitorizar y watch desciptor del evento
+    char buffInotify[BUFF_SIZE];     // Buffer de la informacion de InotifyEvent
+    ssize_t lengthRd;               // Longitud del evento leido
+    pid_t pid;
+
+    // Crea una instancia de Inotify
+    fdDirectory = inotify_init();
+    if (fdDirectory == -1) {
+        fprintf(stderr, "[INotify-error]: Creacion de Inotify Fallida. %d: %s \n", errno, strerror(errno));
+        exit(-1);
+    }
+    printf("[INotify]: Se ha Creado Inotify\n");
+
+    addWatchToTree(fdDirectory, _route, wdCreate);
+
+    // Ciclo que monitorea constante mente el directorio
+    while(1){
+        // Espera a que ocurra un evento y guarda la informacion y su longitud
+        lengthRd = read(fdDirectory, buffInotify, BUFF_SIZE);
+        if(lengthRd <= 0){
+            fprintf(stderr, "[INotify-error]: La lectura de inotify es <= 0.\n");
+            exit(-1);
+        }
+        printf("[INotify]: Se ha detectado un cambio.\n");
+
+        pid = fork();
+        if(pid == 0){
+            //close(fdDirectory);
+
+            printf("[INotify]: El archivo creado es: %s \n", ((struct inotify_event*)buffInotify)->name );
+
+            clientSocket(_ip, _port, _route, ((struct inotify_event*)buffInotify)->name, fdDirectory, wdCreate);
+
+            printf("Saliendo del proceso.\n");
+            exit(0);
+        }
+    }
+    close(fdDirectory);
+}
+
+
+
+
+void clientSocket(char *_ip, int _port, char *_routeRoot, char *name, int fdDirectory, int wdCreate){
+
+    int fdSocket;                       // File Descriptor del socket
+    struct sockaddr_in serverAddr;      // Direccion del Socket del Servidor
+    char buffRx[BUFF_SIZE];              // Buffer de Entrada
+
+    // Creacion del Socket
+    if ((fdSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1){
+        printf("[CLIENTE]: Fallo la Creacion del Cliente.\n");
+        exit(-1);
+    }
+    printf("[CLIENTE]: El Socket se Creo Satisfactoriamente.\n");
+
+    // Limpia el socket
+    memset(&serverAddr, 0, sizeof(serverAddr));
+
+    // Asigna la Familia, IPv4, Puerto
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = inet_addr( _ip );
+    serverAddr.sin_port = htons( _port );
+
+    char _absoluteRoute[BUFF_SIZE] = {0};
+    buscarArchivo(_routeRoot, name, _absoluteRoute);
+
+    char _routeTemp1[BUFF_SIZE];
+    char _absoluteTemp1[BUFF_SIZE];
+    char _relativeRoute[BUFF_SIZE] = {0};
+
+    char delim1[2] = "/";
+    char *tokenOld;
+    char *tokenNew;
+
+    strcpy(_routeTemp1, _routeRoot);
+    strcpy(_absoluteTemp1, _absoluteRoute);
+    tokenNew = strtok(_routeTemp1, delim1);
+    while(tokenNew != NULL){
+        tokenOld = tokenNew;
+        tokenNew = strtok(NULL, delim1);
+    }
+
+    tokenNew = strtok(_absoluteTemp1, delim1);
+    bool flag = false;
+    while(tokenNew != NULL){
+        if((strcmp(tokenNew, tokenOld)==0)){
+            flag = true;
+        } else if(flag){
+            strcat(_relativeRoute, "/");
+            strcat(_relativeRoute, tokenNew);
+        }
+        tokenNew = strtok(NULL, delim1);
+    }
+
+    printf("[CLIENTE]: Ruta absoluta del archivo: %s\n", _absoluteRoute);
+    printf("[CLIENTE]: Ruta relativa del archivo: %s\n", _relativeRoute);
+    // Abre el archivo creado en modo lectura binaria
+    FILE *fileOut = fopen(_absoluteRoute,"rb");
+    if(fileOut == NULL){
+		    printf("[CLIENTE-error]: Archivo no encontrado\n");
+		    exit(-1);
+	  }
+    printf("[CLIENTE]: Archivo creado abierto exitosamente. \n");
+
+    DIR *isDir = opendir(_absoluteRoute);
+    if (!(isDir = opendir(_absoluteRoute))){
+        printf("[CLIENTE]: Es un archivo\n");
+    } else {
+        addWatchToTree(fdDirectory, _absoluteRoute, wdCreate);
+        strcat(_relativeRoute, "/");
+        printf("[CLIENTE]: Es directorio\n");
+    }
+    closedir(isDir);
+
+    // Intenta conectarse al socket del servidor
+    if (connect(fdSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) != 0){
+        fprintf(stderr, "[CLIENTE-error]: No Pudo Conectar el Socket. %d: %s \n", errno, strerror( errno ));
+        exit(-1);
+    }
+    printf("[CLIENTE]: Conexion establecida con %s.\n", (char *)inet_ntoa(serverAddr.sin_addr));
+
+    enviarArchivo(fdSocket, fileOut, _relativeRoute);
+
+    printf("[CLIENTE]: Cerrando conexion.\n");
+    close(fdSocket);
+
+    printf("[CLIENTE]: Cerrando Archivo.\n");
+    fclose (fileOut);
+}
+
+
+
+
+void buscarArchivo(char *routeRoot, char *name, char *_route){
+    DIR *dir;
+    struct dirent *entry;
+
+    if (!(dir = opendir(routeRoot))){
+        fprintf(stderr, "[INotify-error]: No se pudo Abrir el directorio. %d: %s \n", errno, strerror(errno));
+    } else {
+        while ((entry = readdir(dir)) != NULL) {
+            if(entry->d_type == DT_DIR){
+                char path[BUFF_SIZE];
+                if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+                    continue;
+                snprintf(path, sizeof(path), "%s/%s", routeRoot, entry->d_name);
+
+                //printf("\nDirectorio: %s:%s", entry->d_name, name);
+                if(strcmp(entry->d_name, name) == 0){
+                    strcpy(_route, path);
+                    break;
+                } else {
+                    buscarArchivo(path, name, _route);
+                    if(_route[0] != 0) break;
+                }
+            } else {
+                char path[BUFF_SIZE];
+                snprintf(path, sizeof(path), "%s/%s", routeRoot, entry->d_name);
+                //printf("\nArchivo: %s:-%s", path, entry->d_name);
+                if(strcmp(entry->d_name, name) == 0){
+                    strcpy(_route, path);
+                    break;
+                }
+            }
+        }
+        closedir(dir);
+    }
+}
+
+
+
+
+
+void addWatchToTree(int fdDirectory, char *_route, int wdCreate){
+    DIR *dir;
+    struct dirent *entry;
+
+    if (!(dir = opendir(_route))){
+            fprintf(stderr, "[INotify-error]: No se pudo Abrir el directorio. %d: %s \n", errno, strerror(errno));
+    } else if(_route != ""){
+        // Crea un Watch Descriptor para la ruta dada
+        wdCreate = inotify_add_watch(fdDirectory, _route, IN_CREATE);
+        if (wdCreate == -1) {
+            fprintf(stderr, "[INotify-error]: No se pudo añadir el Watch. %d: %s \n", errno, strerror(errno));
+            exit(-1);
+        }
+        printf("[INotify]: Se ha añadodo watch a %s \n", _route );
+
+        while ((entry = readdir(dir)) != NULL) {
+            char _routeTemp[BUFF_SIZE] = {0};
+            if (entry->d_type == DT_DIR) {
+                char path[BUFF_SIZE];
+                if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+                    continue;
+                snprintf(path, sizeof(path), "%s/%s", _route, entry->d_name);
+                addWatchToTree(fdDirectory, path, wdCreate);
+            }
+        }
+        closedir(dir);
+    }
+}
+
+
+
+
+void enviarArchivo(int fdConnection, FILE *fdFile, char *name){
+    char data[BUFF_SIZE] = {0};
+    char msj[BUFF_SIZE] = {0};
+
+    printf("[CLIENTE]: Mandando ruta de archivo.\n");
+    write(fdConnection, name, BUFF_SIZE);
+    read(fdConnection, msj, BUFF_SIZE);
+    printf("[SERVIDOR]: %s.\n");
+
+    printf("[CLIENTE]: Iniciando Transferencia\n");
+    while(fgets(data, BUFF_SIZE, fdFile) != NULL) {
+        if (send(fdConnection, data, sizeof(data), 0) == -1) {
+            perror("[CLIENTE]: Error al mandar el archivo.");
+            exit(-1);
+        }
+        bzero(data, BUFF_SIZE);
+    }
+    printf("[CLIENTE]: Transferencia Completada\n");
+    close(fdConnection);
+}
+
+
+
+
+
+
+
--- SistemasOperativos/serverSocket.c
+++ SistemasOperativos/serverSocket.c
@@ -1,4 +1,4 @@
-//Simbolos Estandar y salir 
+//Estandar
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -17,162 +17,48 @@
 //Monitorizacion
 #include <sys/inotify.h>
 
-#define BUFF_MAX 256
-
-#define READ_END  0
-#define WRITE_END 1
-
-void clientSocket(char _ip[], int _port, char _msg[]){
-    int fdSocket;                       // File Descriptor del socket
-    struct sockaddr_in serverAddr;      // Direccion del Socket del Servidor 
- 
-    char buffRx[BUFF_MAX];              // Buffer de Entrada 
-    
-
-    // Creacion del Socket
-    if ((fdSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1){ 
-        printf("[CLIENTE]: Fallo la Creacion del Cliente.\n"); 
-        exit(-1);  
-    }
-    printf("[CLIENTE]: El Socket se Creo Satisfactoriamente.\n"); 
-    
-
-    // Limpia el socket 
-    memset(&serverAddr, 0, sizeof(serverAddr));
+//Directorios
+#include <sys/stat.h>
+#include <dirent.h>
 
 
-    // Asigna la Familia, IPv4, Puerto
-    serverAddr.sin_family = AF_INET; 
-    serverAddr.sin_addr.s_addr = inet_addr( _ip ); 
-    serverAddr.sin_port = htons( _port ); 
- 
 
-    // Intenta conectarse al socket del servidor
-    if (connect(fdSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) != 0){ 
-        fprintf(stderr, "[CLIENTE-error]: No Pudo Conectar el Socket. %d: %s \n", errno, strerror( errno ));
-        exit(-1);
-    } 
-    printf("[CLIENTE]: Enviando mensaje al Servidor.\n"); 
-    write(fdSocket, _msg, BUFF_MAX);     // Envia un Mensaje al Servidor
-      
-    printf("[CLIENTE]: Cerrando Coneccion.\n");
-    // Cierra la Conexion con el Servidor
-    close(fdSocket);
+
+#define BUFF_SIZE 1024
+
+
+
+void crearDirectorio(char *routeRoot, char *route);
+void crearArchivo(int fdConnection, char* routeRoot);
+void socketServer(char _ip[], int _port, int _backlog, char _route[]);
+
+
+/**
+ * argv[1] = IP
+ * argv[2] = PUERTO
+ * argv[3] = BACKLOG
+ * argv[4] = RUTA
+ */
+int main(int argc, char ** argv){
+    socketServer(argv[1], atoi(argv[2]), atoi(argv[3]), argv[4]);
+    exit(0);
 }
 
 
 
-void directoryMonitor(char _ip[], int _port, char _route[], int _pipeFlag[2] ){
-    int fdDirectory, wdCreate;      // File Descriptor de la carpeta a monitorizar y watch desciptor del evento
-    char buffInotify[BUFF_MAX];     // Buffer de la informacion de InotifyEvent
-    ssize_t lengthRd;               // Longitud del evento leido  
-    pid_t pid;
-
-    // Crea una instancia de Inotify 
-    fdDirectory = inotify_init();
-    if (fdDirectory == -1) {
-        fprintf(stderr, "[INotify-error]: Creacion de Inotify Fallida. %d: %s \n", errno, strerror(errno));
-        exit(-1);
-    }
-    printf("[INotify]: Se ha Creado Inotify\n");
-   
-    // Crea un Watch Descriptor para la ruta dada
-    wdCreate = inotify_add_watch(fdDirectory, _route, IN_CREATE);
-    if (wdCreate == -1) {
-        fprintf(stderr, "[INotify-error]: No se pudo añadir el Watch. %d: %s \n", errno, strerror(errno));
-        exit(-1);
-    }
-    printf("[INotify]: Se ha añadodo watch a %s \n", _route );
-    
-    // Ciclo que monitorea constante mente el directorio
-    while(1){
-        // Espera a que ocurra un evento y guarda la informacion y su longitud
-        lengthRd = read(fdDirectory, buffInotify, BUFF_MAX);
-        if(lengthRd <= 0){
-            fprintf(stderr, "[SISTEM-error]: La lectura de inotify es <= 0.\n");
-            exit(-1);
-        }
-
-        pid = fork();
-        if(pid == 0){
-            close(fdDirectory);
-            char _buffOut[BUFF_MAX] = {0};      
-    
-            strcat(_buffOut, ((struct inotify_event*)buffInotify)->name);
-            printf("[INotify]: El archivo creado es: %s \n", _buffOut );
-            
-            //char buffPipe[BUFF_MAX];
-            //close(_pipeFlag[WRITE_END]);
-            //int num = read(_pipeFlag[READ_END], buffPipe, BUFF_MAX);
-            //printf("El archivo que creo el Servidor es: %d \n", num);
-
-            struct arrayChar{ char string[BUFF_MAX]; };
-            struct arrayChar _aux;
-     
-            char *_bufferTemp;
-            int lines = -1;
-
-            FILE *ipsFile = fopen("ipsFile","r");
-            if(ipsFile == NULL){
-		            printf("Archivo no encontrado\n");
-		            exit(-1);
-	          }
-            printf("[INotify]: Archivo de Ips y Puertos abierto exitosamente.\n");
-
-            while(1){
-                lines += 1;
-                _bufferTemp = fgets(_aux.string, BUFF_MAX, ipsFile);
-                if(_bufferTemp == NULL){
-                    printf("[INotify]: Creacion de Hijos completada.\n");
-                    break;
-                }
-                char delim[] = " ";
-                char *token = strtok(_bufferTemp, delim);
-                int flag = 0;
-                char _tmpIp[20];
-                int _tmpPort;
-                if(token != NULL)
-                    while( token  != NULL){
-                        if(flag == 0) { strcpy(_tmpIp, token); flag = 1; }
-                        else { _tmpPort = atoi(token); flag = 0; }
-                        token = strtok(NULL, delim);
-                    }
-                pid = fork();
-                if(pid == 0){
-                    fclose(ipsFile);
-                    printf("[IP] %s \t[PORT]%d\n", _tmpIp, _tmpPort);
-                    if(_tmpPort != _port) clientSocket(_tmpIp, _tmpPort, _buffOut);
-                    exit(0);
-                }
-            }
-            printf("[INotify]: %d direcciones recuperadas exitosamente.\n", lines);
-
-            fclose (ipsFile);
-            exit(0);
-        }   
-    }
-}
-
-
-void socketServer(char _ip[], int _port, int _backlog, char _route[], int _pipeFlag[2]){
+void socketServer(char _ip[], int _port, int _backlog, char _route[]){
 
     int fdSocket, fdConnection;                     // File Descriptors
     struct sockaddr_in serverAddr, clientAddr;      // Formato de direccion del servidor y cliente
-    
     socklen_t clientAddrLen;                        // Tamaño de la direccion del cliente
-    int  lengthRx;                                  // Bytes transmitidos y recibidos
-    
-    char buffRx[BUFF_MAX];                          // Buffer de Recepcion
-    
     pid_t pIdSocket;                                // Id para control del proceso padre e hijo
-    
 
     // Crea y Comprueba si el socket se ha creado exitosamente
     if ( (fdSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1 ) {
         fprintf(stderr, "[SERVIDOR-error]: Creacion de Socket Fallida. %d: %s \n", errno, strerror(errno));
         exit(EXIT_FAILURE);
     }
-    printf("[SERVIDOR]: Socket Creado Satisfactoriamente.\n"); 
+    printf("[SERVIDOR]: Socket Creado Satisfactoriamente.\n");
 
 
     // Inilializa con ceros la estructura
@@ -189,67 +75,39 @@ void socketServer(char _ip[], int _port, int _backlog, char _route[], int _pipeF
         fprintf(stderr, "[SERVIDOR-error]: Enlazamiento de Socket Fallida. %d: %s \n", errno, strerror( errno ));
         exit(EXIT_FAILURE);
     }
-    printf("[SERVIDOR]: Socket Enlazado Satiscatoriamente.\n"); 
+    printf("[SERVIDOR]: Socket Enlazado Satiscatoriamente.\n");
 
 
 
-    // Pone el Socket en modo escuchar (Listen) 
+    // Pone el Socket en modo escuchar (Listen)
     if ((listen(fdSocket, _backlog)) != 0){
         fprintf(stderr, "[SERVIDOR-error]: No Pudo Activar el Socket. %d: %s \n", errno, strerror( errno ));
         exit(EXIT_FAILURE);
-    } 
-    printf("[SERVIDOR]: Escuchando en el Puerto %d \n", ntohs(serverAddr.sin_port) ); 
+    }
+    printf("[SERVIDOR]: Escuchando en el Puerto %d \n", ntohs(serverAddr.sin_port) );
 
 
-    
+
     // Ciclo que espera a que se conecte un cliente y crea un hijo para que lo atienda
     while(1){
-        
+
         // Espera la coneccion de un cliente
         clientAddrLen = sizeof(struct sockaddr_in);
         fdConnection = accept(fdSocket, (struct sockaddr *)&clientAddr, &clientAddrLen);
-        printf("[SERVIDOR]: Conexion Detectada %d.\n", htons(clientAddr.sin_port)); 
+        printf("[SERVIDOR]: Conexion Detectada %s.\n", (char *)inet_ntoa(clientAddr.sin_addr));
 
         if (fdConnection < 0){
             // Error en la conexion
             fprintf(stderr, "[SERVIDOR-error]: Conexion no aceptada. %d: %s \n", errno, strerror( errno ));
         } else {
             pIdSocket = fork();
-            
             if(pIdSocket == 0){   // Proceso hijo que servira al cliente
-                printf("[SERVIDOR-hijo]: Entrando en proceso hijo.\n");
-                while(1){
-                    
-                    // Obtiene el tamaño del mensaje recibido del cliente
-                    lengthRx = read(fdConnection, buffRx, sizeof(buffRx));
-                    buffRx[lengthRx] = '\0';
-                    if(lengthRx == -1){         // Si no se puede leer manda un mensaje de error.
-                        fprintf(stderr, "[SERVIDOR-hijo-error]: fdConnection NO Pudo Ser Leida. %d: %s \n", errno, strerror( errno ));
-                    } else if(lengthRx == 0){   // Si el cliente manda un valor de longitud 0 cierra la coneccion
-                        printf("[SERVIDOR-hijo]: Cliente ha Sido Cerrado, Cerrando fdConnection.\n");
-                        close(fdConnection);
-                        break;
-                    } else{                     // Se lee el mensaje recibido del cliente                    
-                        printf("[SERVIDOR-hijo]: Archivo: %s \n", buffRx);             // Imprime el mensaje recibido del criente
-                    
-                        char _tmpRoute[BUFF_MAX];
-                        strcpy(_tmpRoute, _route);
-                        strcat(_tmpRoute, "/");
-                        strcat(_tmpRoute, buffRx);
-                        
-                        printf("[SISTEMA]: Creando Archivo \'%s\' Creado en \'%s\'\n", buffRx, _route);
-                        int errorExec = execlp("/bin/mkdir", "mkdir", _tmpRoute); 
-                        if(errorExec == -1){
-                            fprintf(stderr, "[SISTEMA-error]: Archivo Existente.\n");
-                        } else {
-                            printf("[SISTEMA]: Archivo Creado con exito.");
-                            //close(_pipeFlag[READ_END]);
-                            //write(_pipeFlag[WRITE_END], buffRx, BUFF_MAX);
-                            //close(_pipeFlag[WRITE_END]);
-                        }
-                    }
-                }
-                printf("[SERVIDOR]: Saliendo de proceso hijo.\n");      
+                crearArchivo(fdConnection, _route);
+
+                printf("[SERVIDOR-hijo]: Cerrando conexion\n");
+                close(fdConnection);
+
+                printf("[SERVIDOR-hijo]: Saliendo de proceso hijo.\n");
                 exit(0);                                     // Sale del proceso hijo
             } else { close(fdConnection); }     // Cierra la coneccion para el padre
         }
@@ -259,34 +117,103 @@ void socketServer(char _ip[], int _port, int _backlog, char _route[], int _pipeF
 }
 
 
-/**
- * argv[1] = IP
- * argv[2] = PUERTO
- * argv[3] = BACKLOG
- * argv[4] = RUTA
- */
-int main(int argc, char ** argv){
-  
-    pid_t pid;
-    pid = fork();
-    
-    int _pipeFlag[2];
-    pipe(_pipeFlag);
 
 
-    if(pid == 0){
-        directoryMonitor(argv[1], atoi(argv[2]), argv[4], _pipeFlag); 
-        exit(0);
+void crearArchivo(int fdConnection, char* routeRoot){
+    int _len;
+    FILE *fdFile;
+    char _buffer[BUFF_SIZE];
+    char route[BUFF_SIZE];
+
+    printf("[SERVIDOR-hijo]: Obteniendo ruta de archivo.\n");
+    read(fdConnection, route, BUFF_SIZE);
+
+    printf("[SERVIDOR-hijo]: Ruta obtenida. %s\n", route);
+    write(fdConnection, "Recibido", BUFF_SIZE);
+
+    char _tempRouteDir[BUFF_SIZE];
+    strcpy(_tempRouteDir,routeRoot);
+    strcat(_tempRouteDir,route);
+
+    int contador = 0;
+    while( route[contador] != 0 ) contador++;
+    contador--;
+    printf("%c", route[contador]);
+    if(route[contador] == '/'){
+        printf("[SERVIDOR-hijo]: Es un directorio.\n");
+        DIR *dirFD = opendir(_tempRouteDir);
+        if(!(dirFD = opendir(_tempRouteDir))){
+            fprintf(stderr, "[SERVIDOR-error]:  Iniciando creacion de directorio. %d: %s \n", errno, strerror(errno));
+            crearDirectorio(routeRoot, route);
+        } else { printf("El directorio ya existe\n"); }
+        closedir(dirFD);
+    } else {
+        printf("[SERVIDOR-hijo]: Es un archivo.\n");
+        fdFile = fopen(_tempRouteDir, "wb");
+        if(fdFile == NULL){
+            fprintf(stderr, "[SERVIDOR-error]: No existe el directorio. %d: %s \n", errno, strerror(errno));
+
+            int index = 0;
+            int indexcut = 0;
+            char routeCut[BUFF_SIZE] = {0};
+            char routeRelative[BUFF_SIZE] = {0};
+
+            while(route[index] != 0){
+                routeCut[indexcut] = route[index];
+                indexcut++;
+                index++;
+                if(route[index] == '/'){
+                    strcat(routeRelative, routeCut);
+                    strcat(routeRelative, "/");
+                    bzero(routeCut, BUFF_SIZE);
+                    indexcut = 0;
+                }
+            }
+            crearDirectorio(routeRoot, routeRelative);
+
+            fdFile = fopen(_tempRouteDir, "wb");
+            if(fdFile == NULL){
+                fprintf(stderr, "[SERVIDOR-error]: No se pudo crear el directorio. %d: %s \n", errno, strerror(errno));
+                fclose(fdFile);
+                exit(-1);
+            }
+        } else {
+            fdFile = fopen(_tempRouteDir, "wb");
+            if(fdFile == NULL){
+                fprintf(stderr, "[SERVIDOR-error]: No se pudo crear el directorio. %d: %s \n", errno, strerror(errno));
+                fclose(fdFile);
+                exit(-1);
+            }
+            fclose(fdFile);
+        }
     }
-    
-    //pid = fork();
-    //if(pid == 0){
-        socketServer(argv[1], atoi(argv[2]), atoi(argv[3]), argv[4], _pipeFlag);
-        exit(0);
-    //} 
-   
-    //char _c[BUFF_MAX];
-    //getline(_c);
-    
-    exit(0);
+}
+
+
+
+
+void crearDirectorio(char *routeRoot, char *route){
+    char _routeTemp[BUFF_SIZE];
+    char delim[2] = "/";
+    char *token1;
+    char *token2;
+    char _routeFoward[BUFF_SIZE] = {0};
+
+    strcpy(_routeFoward, routeRoot);
+    strcat(_routeFoward, "/");
+
+    strcpy(_routeTemp, route);
+
+    token1 = strtok(_routeTemp, delim);
+    while(token1 != NULL){
+        token2 = token1;
+        token1 = strtok(NULL, delim);
+        strcat(_routeFoward, token2);
+        printf("[SERVIDOR]: Creando directorio: %s\n", _routeFoward);
+        if(mkdir(_routeFoward, S_IRWXU) == -1)
+            fprintf(stderr, "[SERVIDOR-error]: Este directorio ya existe. %d: %s \n", errno, strerror(errno));
+        else printf("[SERVIDOR]: Directorio creado.");
+
+        if(token1 != NULL) strcat(_routeFoward, "/");
+    }
 }
